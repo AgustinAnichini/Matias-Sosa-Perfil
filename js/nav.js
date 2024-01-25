0e33334dc933cf8e99af34ca0d6f7a5ff61e9844
Lautaro Salazar <salazarlautaroalejandro@hotmail.com>
2024-01-24 22:31:39 -0300

    cambios responsive de tablet

--- js/nav.js
+++ js/nav.js
@@ -11,3 +11,14 @@ if (window.innerWidth > 767) {
     prevScrollpos = currentScrollPos;
   };
 }
+if(window.innerWidth > 767 && window.innerWidth < 1024){
+  window.onscroll = function () {
+    let currentScrollPos = window.scrollY;
+    if (prevScrollpos > currentScrollPos) {
+      document.querySelector("#nav").style.top = "0px";
+    } else {
+      document.querySelector("#nav").style.top = "-150px";
+    }
+    prevScrollpos = currentScrollPos;
+  };
+}
