06f6fb021b702098767a4ab754e3db89429b06e5
Lautaro Salazar <salazarlautaroalejandro@hotmail.com>
2024-01-30 20:39:41 -0300

    arreglo responsive team.js

--- js/team.js
+++ js/team.js
@@ -16,89 +16,144 @@ team.addEventListener('click', view)
 
 function view(e) {
     const id = e.target.id// obtiene ID
-    switch (id) {
-        case foto1.id:
-            descripcionGeneral.classList.add('equipo-display-none')
-            descripcionCaro.classList.remove('equipo-display-none')
-            // 
-            descripcionAilen.classList.add('equipo-display-none')
-            descripcionNico.classList.add('equipo-display-none')
-            descripcionBrenda.classList.add('equipo-display-none')
-            descripcionArami.classList.add('equipo-display-none')
-            //
-            foto1.classList.add('fotosMovile')
+    if(window.innerWidth>=1024) {
+        switch (id) {
+            case foto1.id:
+                descripcionGeneral.classList.add('equipo-display-none')
+                descripcionCaro.classList.remove('equipo-display-none')
+                // 
+                descripcionAilen.classList.add('equipo-display-none')
+                descripcionNico.classList.add('equipo-display-none')
+                descripcionBrenda.classList.add('equipo-display-none')
+                descripcionArami.classList.add('equipo-display-none')
+                break;
+            case foto2.id:
+                descripcionGeneral.classList.add('equipo-display-none')
+                descripcionCaro.classList.add('equipo-display-none')
+                // 
+                descripcionAilen.classList.remove('equipo-display-none')
+                descripcionNico.classList.add('equipo-display-none')
+                descripcionBrenda.classList.add('equipo-display-none')
+                descripcionArami.classList.add('equipo-display-none')
+    
+                break;
+            case foto3.id:
+                descripcionGeneral.classList.add('equipo-display-none')
+                descripcionCaro.classList.add('equipo-display-none')
+                // 
+                descripcionAilen.classList.add('equipo-display-none')
+                descripcionNico.classList.add('equipo-display-none')
+                descripcionBrenda.classList.remove('equipo-display-none')
+                descripcionArami.classList.add('equipo-display-none')
+                break;
+            case foto4.id:
+                descripcionGeneral.classList.add('equipo-display-none')
+                descripcionCaro.classList.add('equipo-display-none')
+                // 
+                descripcionAilen.classList.add('equipo-display-none')
+                descripcionNico.classList.remove('equipo-display-none')
+                descripcionBrenda.classList.add('equipo-display-none')
+                descripcionArami.classList.add('equipo-display-none')
+                break;
+            case foto5.id:
+                descripcionGeneral.classList.add('equipo-display-none')
+                descripcionCaro.classList.add('equipo-display-none')
+                // 
+                descripcionAilen.classList.add('equipo-display-none')
+                descripcionNico.classList.add('equipo-display-none')
+                descripcionBrenda.classList.add('equipo-display-none')
+                descripcionArami.classList.remove('equipo-display-none')
+                break;
+    
+            default:
+                break;
+        }
+    }
+    else{
+        switch (id) {
+            case foto1.id:
+                descripcionGeneral.classList.add('equipo-display-none')
+                descripcionCaro.classList.remove('equipo-display-none')
+                // 
+                descripcionAilen.classList.add('equipo-display-none')
+                descripcionNico.classList.add('equipo-display-none')
+                descripcionBrenda.classList.add('equipo-display-none')
+                descripcionArami.classList.add('equipo-display-none')
+                //
+                foto1.classList.add('fotosMovile')
 
-            foto4.classList.remove('fotosMovile')
-            foto2.classList.remove('fotosMovile')
-            foto3.classList.remove('fotosMovile')
-            foto5.classList.remove('fotosMovile')
-            break;
-        case foto2.id:
-            descripcionGeneral.classList.add('equipo-display-none')
-            descripcionCaro.classList.add('equipo-display-none')
-            // 
-            descripcionAilen.classList.remove('equipo-display-none')
-            descripcionNico.classList.add('equipo-display-none')
-            descripcionBrenda.classList.add('equipo-display-none')
-            descripcionArami.classList.add('equipo-display-none')
-            //
-            foto2.classList.add('fotosMovile')
+                foto4.classList.remove('fotosMovile')
+                foto2.classList.remove('fotosMovile')
+                foto3.classList.remove('fotosMovile')
+                foto5.classList.remove('fotosMovile')
+                break;
+            case foto2.id:
+                descripcionGeneral.classList.add('equipo-display-none')
+                descripcionCaro.classList.add('equipo-display-none')
+                // 
+                descripcionAilen.classList.remove('equipo-display-none')
+                descripcionNico.classList.add('equipo-display-none')
+                descripcionBrenda.classList.add('equipo-display-none')
+                descripcionArami.classList.add('equipo-display-none')
+                //
+                foto2.classList.add('fotosMovile')
 
-            foto4.classList.remove('fotosMovile')
-            foto1.classList.remove('fotosMovile')
-            foto3.classList.remove('fotosMovile')
-            foto5.classList.remove('fotosMovile')
-            break;
-        case foto3.id:
-            descripcionGeneral.classList.add('equipo-display-none')
-            descripcionCaro.classList.add('equipo-display-none')
-            // 
-            descripcionAilen.classList.add('equipo-display-none')
-            descripcionNico.classList.add('equipo-display-none')
-            descripcionBrenda.classList.remove('equipo-display-none')
-            descripcionArami.classList.add('equipo-display-none')
-            //
-            foto3.classList.add('fotosMovile')
+                foto4.classList.remove('fotosMovile')
+                foto1.classList.remove('fotosMovile')
+                foto3.classList.remove('fotosMovile')
+                foto5.classList.remove('fotosMovile')
+                break;
+            case foto3.id:
+                descripcionGeneral.classList.add('equipo-display-none')
+                descripcionCaro.classList.add('equipo-display-none')
+                // 
+                descripcionAilen.classList.add('equipo-display-none')
+                descripcionNico.classList.add('equipo-display-none')
+                descripcionBrenda.classList.remove('equipo-display-none')
+                descripcionArami.classList.add('equipo-display-none')
+                //
+                foto3.classList.add('fotosMovile')
 
-            foto4.classList.remove('fotosMovile')
-            foto2.classList.remove('fotosMovile')
-            foto1.classList.remove('fotosMovile')
-            foto5.classList.remove('fotosMovile')
-            break;
-        case foto4.id:
-            descripcionGeneral.classList.add('equipo-display-none')
-            descripcionCaro.classList.add('equipo-display-none')
-            // 
-            descripcionAilen.classList.add('equipo-display-none')
-            descripcionNico.classList.remove('equipo-display-none')
-            descripcionBrenda.classList.add('equipo-display-none')
-            descripcionArami.classList.add('equipo-display-none')
-            //
-            foto4.classList.add('fotosMovile')
+                foto4.classList.remove('fotosMovile')
+                foto2.classList.remove('fotosMovile')
+                foto1.classList.remove('fotosMovile')
+                foto5.classList.remove('fotosMovile')
+                break;
+            case foto4.id:
+                descripcionGeneral.classList.add('equipo-display-none')
+                descripcionCaro.classList.add('equipo-display-none')
+                // 
+                descripcionAilen.classList.add('equipo-display-none')
+                descripcionNico.classList.remove('equipo-display-none')
+                descripcionBrenda.classList.add('equipo-display-none')
+                descripcionArami.classList.add('equipo-display-none')
+                //
+                foto4.classList.add('fotosMovile')
 
-            foto1.classList.remove('fotosMovile')
-            foto2.classList.remove('fotosMovile')
-            foto3.classList.remove('fotosMovile')
-            foto5.classList.remove('fotosMovile')
-            break;
-        case foto5.id:
-            descripcionGeneral.classList.add('equipo-display-none')
-            descripcionCaro.classList.add('equipo-display-none')
-            // 
-            descripcionAilen.classList.add('equipo-display-none')
-            descripcionNico.classList.add('equipo-display-none')
-            descripcionBrenda.classList.add('equipo-display-none')
-            descripcionArami.classList.remove('equipo-display-none')
-            //
-            foto5.classList.add('fotosMovile')
+                foto1.classList.remove('fotosMovile')
+                foto2.classList.remove('fotosMovile')
+                foto3.classList.remove('fotosMovile')
+                foto5.classList.remove('fotosMovile')
+                break;
+            case foto5.id:
+                descripcionGeneral.classList.add('equipo-display-none')
+                descripcionCaro.classList.add('equipo-display-none')
+                // 
+                descripcionAilen.classList.add('equipo-display-none')
+                descripcionNico.classList.add('equipo-display-none')
+                descripcionBrenda.classList.add('equipo-display-none')
+                descripcionArami.classList.remove('equipo-display-none')
+                //
+                foto5.classList.add('fotosMovile')
 
-            foto4.classList.remove('fotosMovile')
-            foto2.classList.remove('fotosMovile')
-            foto3.classList.remove('fotosMovile')
-            foto1.classList.remove('fotosMovile')
-            break;
+                foto4.classList.remove('fotosMovile')
+                foto2.classList.remove('fotosMovile')
+                foto3.classList.remove('fotosMovile')
+                foto1.classList.remove('fotosMovile')
+                break;
 
-        default:
-            break;
+            default:
+                break;
+        }
     }
 }
